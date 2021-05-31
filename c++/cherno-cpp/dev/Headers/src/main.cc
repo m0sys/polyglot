a39deb2b5ccfe56a00aac4bc4cb1c4616f0b2473
mhd53 <mhd53@users.noreply.github.com>
2021-05-31 08:54:04 -0400

    feat(c++): add gdb + branching tutorial            "

--- c++/cherno-cpp/dev/Headers/src/main.cc
+++ c++/cherno-cpp/dev/Headers/src/main.cc
@@ -3,6 +3,28 @@
 
 int main()
 {
-    Log("Hello World!");
+    int a = 8;
+    a++;
+    const char* str = "Hello";
+    
+    for (int i = 0; i < 5; i++) 
+    {
+        const char chr = str[i];
+        std::cout << chr << std::endl;
+    }
+
+    // Branching.
+    int x = 6;
+    if (x == 5) 
+        Log("Hello World!");
+
+    const char* ptr = nullptr;
+    if (ptr)
+        Log(ptr);
+    else
+        Log("Ptr is null!");
+
+    // NOTE: else if (condition) {} === else { if (condition) {} }
+
     std::cin.get();
 }
